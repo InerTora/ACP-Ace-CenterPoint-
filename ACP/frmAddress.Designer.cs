933b28604f1a53b0ef25dd3c2045683b1cddf9b2
InerTora <mjtura23@gmail.com>
2023-12-10 12:51:31 +0800

    commiteddd

--- ACP/frmAddress.Designer.cs
+++ ACP/frmAddress.Designer.cs
@@ -256,7 +256,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(582, 198);
+            this.dataGridView1.Size = new System.Drawing.Size(692, 198);
             this.dataGridView1.TabIndex = 0;
             // 
             // id
